9e9c9129eddceeec453e94768b08c2b151a111aa
HongYi Liou <t30034a7@gmail.com>
2021-09-09 22:19:06 +0800

    update

--- src/components/Layout/components/Content/index.js
+++ src/components/Layout/components/Content/index.js
@@ -9,6 +9,32 @@ const Content = (props) => {
   const experience = useRef();
   const disPatch = useDispatch();
   disPatch(uploadRefs(experience));
+  const experienceData = [
+    {
+      name: 'Mayo鼎恒數位科技',
+      unit: '薪酬及招募產品發展部　前端工程師',
+      works: '(使用 React 前端框架，開發UI/功能元件，串接後端API，依照規格書開發產品)',
+      year: '2021',
+    },
+    {
+      name: '鼎新電腦台中分公司',
+      unit: '產品研發部　前端工程師',
+      works: '(使用 Angular 前端框架開發產品功能，實現UI設計圖，串接後端API，依照規格書開發產品)',
+      year: '2019',
+    },
+    {
+      name: '格子科技',
+      unit: '網頁設計部　網頁設計師',
+      works: '(網頁切版，將設計圖轉換成網頁程式碼，於此時精通了HTML/CSS)',
+      year: '2018',
+    },
+    {
+      name: '私立南台科技大學',
+      unit: '工業管理系　碩士',
+      works: null,
+      year: '2014 ~ 2016',
+    },
+  ];
 
   return (
     <div className={contentClass} ref={experience}>
@@ -19,14 +45,19 @@ const Content = (props) => {
         </article>
         <article className="timelineBlock">
           <ul className="timeline">
-            <li className="item">
-              私立南台科技大學
-              <div className="unit">工業管理系</div>
-              <div className="post">碩士</div>
-            </li>
-            <li className="item">格子科技</li>
-            <li className="item">鼎新電腦台中分公司</li>
-            <li className="item">Mayo鼎恒數位科技</li>
+            {
+              experienceData.map(d => {
+                return (
+                  <li className="item">
+                    {d.name}
+                    <p className="unit">{d.unit}</p>
+                    <p className="works">{d.works || null}</p>
+                    <p className="year">{d.year}</p>
+                    <div className="line" />
+                  </li>
+                );
+              })
+            }
           </ul>
         </article>
       </div>
